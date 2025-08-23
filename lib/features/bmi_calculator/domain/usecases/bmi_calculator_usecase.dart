df80400fc438e7ded186c8790b1b08458f009209
TahaMike <bit21it07@gmail.com>
2025-08-23 11:06:56 +0530

    second commit

--- lib/features/bmi_calculator/domain/usecases/bmi_calculator_usecase.dart
+++ lib/features/bmi_calculator/domain/usecases/bmi_calculator_usecase.dart
@@ -4,8 +4,9 @@ import '../repository/bmi_calculator_repository.dart';
 class BmiCalculatorUseCase{
   final BmiCalculatorRepository _bmiCalculatorRepository;
   BmiCalculatorUseCase(this._bmiCalculatorRepository);
-
+// hello
   dynamic calculateBmi(double weight, double height) {
+    print(" ");
     return _bmiCalculatorRepository.calculateBmi(weight, height);
   }
 }
